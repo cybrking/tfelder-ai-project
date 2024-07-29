f6897dc6f0ed417a961e67f4900f7079af7c6cd7
cybrking <travisfelder@outlook.com>
2024-07-29 21:52:26 +0000

    Apply bug fixes suggested by OpenAI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,74 +1,23 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
-import altair as alt
-import time
-import zipfile
+# Change @st.cache_data to @st.cache
+@st.cache
+def convert_df(input_df):
+    return input_df.to_csv(index=False).encode('utf-8')
 
-# Page title
-st.set_page_config(page_title='ML Model Building', page_icon='🤖')
-st.title('🤖 ML Model Building')
+# Define parameter_max_features_metric based on the selected parameter_max_features
+parameter_max_features_metric = X.shape[1] if parameter_max_features == 'all' else parameter_max_features
 
-with st.expander('About this app'):
-    st.markdown('**What can this app do?**')
-    st.info('This app allow users to build a machine learning (ML) model in an end-to-end workflow. Particularly, this encompasses data upload, data pre-processing, ML model building and post-model analysis.')
+# Update the parameter_criterion options to align with RandomForestRegressor
+parameter_criterion_options = ['mse', 'mae', 'friedman_mse']
+parameter_criterion = parameter_criterion_options[0] if parameter_criterion == 'squared_error' else parameter_criterion
 
-    st.markdown('**How to use the app?**')
-    st.warning('To engage with the app, go to the sidebar and 1. Select a data set and 2. Adjust the model parameters by adjusting the various slider widgets. As a result, this would initiate the ML model building process, display the model results as well as allowing users to download the generated models and accompanying data.')
+# Transpose the rf_results DataFrame correctly
+rf_results = pd.DataFrame({
+    'Method': ['Random forest'],
+    f'Training {parameter_criterion.capitalize()}': [train_mse],
+    'Training R2': [train_r2],
+    f'Test {parameter_criterion.capitalize()}': [test_mse],
+    'Test R2': [test_r2]
+})
 
-    st.markdown('**Under the hood**')
-    st.markdown('Data sets:')
-    st.code('''- Drug solubility data set
-    ''', language='markdown')
-
-    st.markdown('Libraries used:')
-    st.code('''- Pandas for data wrangling
-- Scikit-learn for building a machine learning model
-- Altair for chart creation
-- Streamlit for user interface
-    ''', language='markdown')
-
-# Sidebar for accepting input parameters
-with st.sidebar:
-    # Load data
-    st.header('1.1. Input data')
-
-    st.markdown('**1. Use custom data**')
-    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file, index_col=False)
-
-    # Download example data
-    @st.cache
-    def convert_df(input_df):
-        return input_df.to_csv(index=False).encode('utf-8')
-
-    example_csv = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
-    csv = convert_df(example_csv)
-    st.download_button(
-        label="Download example CSV",
-        data=csv,
-        file_name='delaney_solubility_with_descriptors.csv',
-        mime='text/csv'
-    )
-
-    # Select example data
-    st.markdown('**1.2. Use example data**')
-    example_data = st.toggle('Load example data')
-    if example_data:
-        df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
-
-    st.header('2. Set Parameters')
-    parameter_split_size = st.slider('Data split ratio (% for Training Set)', 10, 90, 80, 5)
-
-    st.subheader('2.1. Learning Parameters')
-    with st.expander('See parameters'):
-        parameter_n_estimators = st.slider('Number of estimators (n_estimators)', 0, 1000, 100, 100)
-        parameter_max_features = st.select_slider('Max features (max_features)', options=['auto', 'sqrt', 'log2'])
-        parameter_min_samples_split = st.slider('Minimum number of samples required to split an internal node (min_samples_split)', 2, 10, 2, 1)
-        parameter_min_samples_leaf = st.slider('Minimum number of samples required to be at a leaf node (min_samples_leaf)', 1, 10, 2, 1)
-
-    st.subheader('2.2
+# Reset the index for df_prediction after concatenation
+df_prediction = pd.concat([df_train, df_test], axis=0).reset_index(drop=True)
